e4621c13b7c0aaa1565a7b768c069a8549d02d13
hatemsallam <hatemsallam94@hotmail.com>
2021-11-01 02:20:39 +0200

    finish challenge26

--- java/sort/app/src/test/java/sort/AppTest.java
+++ java/sort/app/src/test/java/sort/AppTest.java
@@ -3,12 +3,25 @@
  */
 package sort;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+  App app;
+
+  @BeforeEach
+  public void init() {
+    app = new App();
+  }
+
+  @Test
+  public void testExpected() {
+    int arr[] = {15, 2, 6, 3, 1};
+    app.insertionSort(arr);
+    for (int i = 0; i < arr.length - 1; i++) {
+      Assertions.assertTrue(arr[i] <= arr[i + 1], "Returns true if sorted");
     }
+  }
 }
